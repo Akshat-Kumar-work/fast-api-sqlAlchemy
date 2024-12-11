e7c37ad59969695f778b001fabeaa9c8557859ee
Akshat <iamakku0.0.0.1@gmail.com>
2024-12-11 12:29:40 +0530

    some more api's

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
-
+from pydantic   import BaseModel
 
 app = FastAPI()
+
+class Person(BaseModel):
+    firstName: str
+    lastName: str
+    isMale: bool
 
 @app.get('/',status_code=200)
 def getCar_info():
     return{"message":"server is running"}
+
+#f string is used to embed the expression inside the string literals using {}
+@app.get('/getpersonByName/{personName}',status_code=200)
+def getPerson_By_Id(personName:str):
+    return{"message":f"your person name is {personName}"}
+
+
+@app.post('/addPersonInfo',status_code=200)
+def addPerson_info(person:Person):
+    return (person)
+
