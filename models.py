23e831089ead41f3bbca03650276c5d25abe662b
Akshat <iamakku0.0.0.1@gmail.com>
2024-12-23 23:11:38 +0530

    db schema

--- models.py
+++ models.py
@@ -5,9 +5,13 @@ from sqlalchemy import String,Integer,Column,Boolean
 from database import Base
 
 
+#inheriting base to Person class 
+# Inheriting from base ensure that person class is recognized by sql-alchemy
 class Person(Base):
     __tablename__='person'
-    id=Column(Integer,primary_key=True)
+    id=Column(Integer,primary_key=True,autoincrement=True)
     firstName= Column(String(40),nullable=False)
     lastName=Column(String(40),nullable=False)
     isMale=Column(Boolean)
+    password=Column(String)
+    mobileNumber = Column(String(15))
